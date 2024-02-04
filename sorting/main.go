fba551e8abbcc6c219ef3019f3fac28f58162a18
jylkkakai <kiitsu74@gmail.com>
2024-02-04 16:12:52 +0200

    Timing function for sorts.

--- sorting/main.go
+++ sorting/main.go
@@ -15,6 +15,64 @@ func createRndArr(arr []int) {
 	
 }
 
+func timeSort(size int, iter int) {
+
+	rnd_arr := make([]int, size)
+	//start := 0
+	fmt.Printf("Running sort algorithms for %d times...\n", iter)
+	fmt.Printf("Array size: %d\n", size)
+	fmt.Println("Bubble")
+	elapsed_time := 0.0
+	for i := 0; i < iter; i++ {
+		createRndArr(rnd_arr)
+		start := time.Now()
+		bubble(rnd_arr)
+		elapsed_time += float64(time.Since(start))
+	}
+	fmt.Printf("Search time: %f ms\n", elapsed_time/1000000/float64(iter))
+	
+	fmt.Println("Selection")
+	elapsed_time = 0
+	for i := 0; i < iter; i++ {
+		createRndArr(rnd_arr)
+		start := time.Now()
+		selection(rnd_arr)
+		elapsed_time += float64(time.Since(start))
+	}
+	fmt.Printf("Search time: %f ms\n", elapsed_time/1000000/float64(iter))
+
+	fmt.Println("Insertion")
+	elapsed_time = 0
+	for i := 0; i < iter; i++ {
+		createRndArr(rnd_arr)
+		start := time.Now()
+		insertion(rnd_arr)
+		elapsed_time += float64(time.Since(start))
+	}
+	fmt.Printf("Search time: %f ms\n", elapsed_time/1000000/float64(iter))
+
+	fmt.Println("Merge")
+	elapsed_time = 0
+	for i := 0; i < iter; i++ {
+		createRndArr(rnd_arr)
+		start := time.Now()
+		merge(rnd_arr)
+		elapsed_time += float64(time.Since(start))
+	}
+	fmt.Printf("Search time: %f ms\n", elapsed_time/1000000/float64(iter))
+
+	fmt.Println("Quick")
+	elapsed_time = 0
+	for i := 0; i < iter; i++ {
+		createRndArr(rnd_arr)
+		start := time.Now()
+		quick(rnd_arr, 0, size - 1)
+		elapsed_time += float64(time.Since(start))
+	}
+	fmt.Printf("Search time: %f ms\n", elapsed_time/1000000/float64(iter))
+
+}
+
 func main() {
 	fmt.Println("Hello World")
 	size := 20 
@@ -50,7 +108,15 @@ func main() {
 	quick(rnd_arr, 0, size - 1)
 	fmt.Println(rnd_arr)
 
-	fmt.Println(time.Now())
+	timeSort(10, 1000)
+	timeSort(100, 1000)
+	timeSort(1000, 1000)
+	timeSort(10000, 100)
+	timeSort(100000, 100)
+
+
+
+
 }
 
 
