bcf8da12ba98841de4a4234d5a6d7eb459c3619d
jylkkakai <kiitsu74@gmail.com>
2024-02-05 14:14:52 +0200

    Add some basic linked list methods.

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -1,4 +1,77 @@
 package main
 
+import (
+	"fmt"
+)
+
+type Node struct {
+	data int
+	next *Node
+	prev *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (list *LinkedList) push(data int) {
+	newNode := &Node{data: data}
+	if list.head == nil {
+		list.head = newNode
+	} else {
+		current := list.head
+		for current.next != nil {
+			current = current.next
+		}
+		newNode.prev = current
+		current.next = newNode
+		// fmt.Println(current.next)
+	}
+}
+
+func (list *LinkedList) pop () {
+	if list.head == nil {
+		fmt.Println("Empty list!")
+	} else {
+		current := list.head
+		for current.next != nil {
+			current = current.next
+		}
+		current = current.prev
+		current.next = nil
+	}
+}
+
+func (list *LinkedList) sort() {
+	if list.head == nil {
+		fmt.Println("Empty list!")
+	}
+
+}
+
+func (list *LinkedList) printList() {
+	if list.head == nil {
+		fmt.Println("Empty list.")
+	} else {
+		current := list.head
+		for current.next != nil {
+			fmt.Print(current.data, " ")
+			current = current.next
+		}
+		fmt.Print(current.data, "\n")
+	}
+}
 
 
+func (list *LinkedList) printListAll() {
+	if list.head == nil {
+		fmt.Println("Empty list.")
+	} else {
+		current := list.head
+		for current.next != nil {
+			fmt.Println(current)
+			current = current.next
+		}
+		fmt.Println(current)
+	}
+}
--- linked_list/main.go
+++ linked_list/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-		"fmt"
+	"fmt"
+	"math/rand"
 		)
 
 func main () {
 
 	fmt.Println("Hello World!")
-
+	list := &LinkedList{}
+	
+	list.printList()
+	list.pop()
+	list.push(10)
+	list.push(32)
+	list.printList()
+	for i := 0; i < 10; i++ {
+		list.push(rand.Intn(100))
+	}
+	list.printList()
+	fmt.Println(list)
+	list.printListAll()
+	list.pop()
+	list.printList()
+	fmt.Println(list)
+	list.printListAll()
 }
