87300a839bda0190ac002ce6293f333171958641
jylkkakai <kiitsu74@gmail.com>
2024-02-14 17:32:28 +0200

    Update mina.

--- calculator_gio/main.go
+++ calculator_gio/main.go
@@ -1,47 +1,62 @@
-package main
-//
-//import (
-//	"fmt"
-//	//"image/color"
-//	//"os"
-//	//"log"
-//	"gioui.org/app"
-//	//"gioui.org/io/system"
-//	//"gioui.org/layout"
-//	//"gioui.org/op"
-//	//"gioui.org/text"
-//	//"gioui.org/widget/material"
-//
-//)
-//
-//
-//func main () {
-//
-//	fmt.Println("Hello World!")
-//
-//	go func() {
-//		w := app.NewWindow()
-//		
-//		for range w.Events() {
-//		}
-//	}()
-//	app.Main()
-//}
-import (
-  "gioui.org/app"
+
+package main 
+import ( 
+    // "fmt" 
+    
+    //"image/color" 
+    //"image/draw" 
+    "log"
+    "os"
+
+    "gioui.org/app"
+    // "gioui.org/internal/ops"
+    "gioui.org/io/system"
+
+    // //"gioui.org/layout"
+    "gioui.org/op"
+    // //"gioui.org/text"
+    //"gioui.org/widget/material"
 )
 
-func main() {
-  go func() {
-    // create new window
-    w := app.NewWindow()
 
-    // listen for events in the window.
+func main() {
+    go func() {
+
+	w := app.NewWindow()
+	err := run(w)
+	if err != nil {
+	    log.Fatal(err)
+	}
+	os.Exit(0)
+
+    
+    }()
+    app.Main()
+}
+
+func run(window *app.Window) error {
+
+    //////th := material.NewTheme()
+    var ops op.Ops
     for { 
-	w.NextEvent()
+	switch nEvent := window.NextEvent().(type) {
+
+	case system.DestroyEvent:
+	    return nEvent.Err
+
+	case system.FrameEvent:
+	    log.Println("Hello World!")
+
+	    //title := material.H1(th, "Hello Gio!")
+
+	    ops.Reset()
+
+	    //draw(&ops)
+
+	    nEvent.Frame(&ops)
+
+	} 
 			
     }
-  }()
-  app.Main()
 }
 
