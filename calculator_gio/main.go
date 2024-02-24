6cae30b6a797bdcc6c0056be4c9f7702c4888cbb
jylkkakai <kiitsu74@gmail.com>
2024-02-24 20:43:37 +0200

    Basic operations added.

--- calculator_gio/main.go
+++ calculator_gio/main.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-	// "fmt"
+    "fmt"
 
-	"image"
-	"strings"
+    "image"
+    "strings"
+    "strconv"
 
-	//"image/draw"
-	"image/color"
-	"log"
-	"os"
+    //"image/draw"
+    "image/color"
+    "log"
+    "os"
 
-	"gioui.org/app"
-	//"golang.org/x/exp/shiny/text"
-	//"gioui.org/font/gofont"
-	"gioui.org/layout"
-	"gioui.org/op/clip"
-	"gioui.org/op/paint"
-	"gioui.org/text"
+    "gioui.org/app"
+    //"golang.org/x/exp/shiny/text"
+    //"gioui.org/font/gofont"
+    "gioui.org/layout"
+    "gioui.org/op/clip"
+    "gioui.org/op/paint"
+    "gioui.org/text"
 
-	// "gioui.org/internal/ops"
-	//"gioui.org/io/system"
-	"gioui.org/unit"
+    // "gioui.org/internal/ops"
+    //"gioui.org/io/system"
+    "gioui.org/unit"
 
-	// //"gioui.org/layout"
-	"gioui.org/op"
-	// //"gioui.org/text"
-	"gioui.org/widget"
-	"gioui.org/widget/material"
+    // //"gioui.org/layout"
+    "gioui.org/op"
+    // //"gioui.org/text"
+    "gioui.org/widget"
+    "gioui.org/widget/material"
 )
 
 
@@ -69,9 +70,9 @@ func run(window *app.Window) error {
     var btnsub = new(widget.Clickable)
     var btnmul = new(widget.Clickable)
     var btndiv = new(widget.Clickable)
-    var strabove string = " "
-    var strbelow string = " "
-    //inset := layout.Inset{Left: 10, Right: 10}
+    var strArr = make([]string, 0)
+    var strAbove = ""
+    var strBelow string = ""
     
     for { 
 	switch nEvent := window.NextEvent().(type) {
@@ -80,7 +81,6 @@ func run(window *app.Window) error {
 	    return nEvent.Err
 	
 	case app.FrameEvent:
-	    //log.Println("Hello World!") 
 	    gtx := app.NewContext(&ops,  nEvent)
 	    layout.Flex{ 
 		Axis: layout.Vertical,
@@ -89,8 +89,11 @@ func run(window *app.Window) error {
 		layout.Flexed( 0.2, func(gtx layout.Context) layout.Dimensions { 
 		    //drawTextBox(&gtx)
 		    return layout.Inset{Top: 20, Bottom: 0, Left: 10, Right: 10}.Layout(gtx,  func(gtx layout.Context) layout.Dimensions { 
-		    strabove = "Above"
-		    textabove := material.Body1(th, strabove)
+			strAbove = ""
+			for _, s := range strArr {
+			   strAbove += s 
+			}
+		    textabove := material.Body1(th, strAbove)
 		    textabove.Alignment = text.End
 		    textabove.TextSize = 24
 		    return textabove.Layout(gtx)
@@ -98,9 +101,9 @@ func run(window *app.Window) error {
 		    },
 		),
 		layout.Flexed(0.2, func(gtx layout.Context) layout.Dimensions { 
-		    //strbelow = "Below"
+		    //strBelow = "Below"
 		    return layout.Inset{Top: 0, Bottom: 0, Left: 10, Right: 10}.Layout(gtx,  func(gtx layout.Context) layout.Dimensions {
-		    textbelow := material.Body1(th, strbelow)
+		    textbelow := material.Body1(th, strBelow)
 		    textbelow.MaxLines = 1
 		    textbelow.Alignment = text.End
 		    textbelow.TextSize = 24
@@ -112,36 +115,44 @@ func run(window *app.Window) error {
 		    func(gtx layout.Context) layout.Dimensions {
 			return layout.Flex{Spacing: layout.SpaceAround}.Layout(gtx,
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn7.Clicked(gtx){
-					log.Println("7")
+					strBelow = strBelow + "7"
 				    } 
 				    tbtn := material.Button(th, btn7, "7")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn8.Clicked(gtx){
-					log.Println("8")
+					strBelow = strBelow + "8"
 				    } 
 				    tbtn := material.Button(th, btn8, "8")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn9.Clicked(gtx){
-					log.Println("9")
+					strBelow = strBelow + "9"
 				    } 
 				    tbtn := material.Button(th, btn9, "9")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btndiv.Clicked(gtx){
-					log.Println("/")
+					if len(strBelow) > 0 {
+					    if len(strArr) == 1 {
+						strArr = make([]string, 0)
+					    }
+					    strArr = append(strArr, strBelow)
+					    strArr = append(strArr, "/")
+					    strBelow = ""
+					    strArr = calculate(strArr)
+					}
 				    } 
 				    tbtn := material.Button(th, btndiv, "/")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
@@ -155,36 +166,44 @@ func run(window *app.Window) error {
 		    func(gtx layout.Context) layout.Dimensions {
 			return layout.Flex{Spacing: layout.SpaceAround}.Layout(gtx,
 			    layout.Flexed(0.2, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn4.Clicked(gtx){
-					log.Println("4")
+					strBelow = strBelow + "4"
 				    } 
 				    tbtn := material.Button(th, btn4, "4")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.2,func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn5.Clicked(gtx){
-					log.Println("5")
+					strBelow = strBelow + "5"
 				    } 
 				    tbtn := material.Button(th, btn5, "5")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.2,  func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn6.Clicked(gtx){
-					log.Println("6")
+					strBelow = strBelow + "6"
 				    } 
 				    tbtn := material.Button(th, btn6, "6")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.2,  func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btnmul.Clicked(gtx){
-					log.Println("x")
+					if len(strBelow) > 0 {
+					    if len(strArr) == 1 {
+						strArr = make([]string, 0)
+					    }
+					    strArr = append(strArr, strBelow)
+					    strArr = append(strArr, "x")
+					    strBelow = ""
+					    strArr = calculate(strArr)
+					}
 				    } 
 				    tbtn := material.Button(th, btnmul, "x")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
@@ -198,36 +217,44 @@ func run(window *app.Window) error {
 		    func(gtx layout.Context) layout.Dimensions {
 			return layout.Flex{Spacing: layout.SpaceAround}.Layout(gtx,
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn1.Clicked(gtx){
-					log.Println("1")
+					strBelow = strBelow + "1"
 				    } 
 				    tbtn := material.Button(th, btn1, "1")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn2.Clicked(gtx){
-					log.Println("2")
+					strBelow = strBelow + "2"
 				    } 
 				    tbtn := material.Button(th, btn2, "2")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn3.Clicked(gtx){
-					log.Println("3")
+					strBelow = strBelow + "3"
 				    } 
 				    tbtn := material.Button(th, btn3, "3")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btnsub.Clicked(gtx){
-					log.Println("-")
+					if len(strBelow) > 0 {
+					    if len(strArr) == 1 {
+						strArr = make([]string, 0)
+					    }
+					    strArr = append(strArr, strBelow)
+					    strArr = append(strArr, "-")
+					    strBelow = ""
+					    strArr = calculate(strArr)
+					}
 				    } 
 				    tbtn := material.Button(th, btnsub, "-")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
@@ -241,11 +268,10 @@ func run(window *app.Window) error {
 		    func(gtx layout.Context) layout.Dimensions {
 			return layout.Flex{Spacing: layout.SpaceAround}.Layout(gtx,
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btndot.Clicked(gtx){
-					log.Println(".")
-					if !strings.Contains(strbelow, ".") {
-					    strbelow = strbelow + "."
+					if !strings.Contains(strBelow, ".") {
+					    strBelow = strBelow + "."
 					}
 				    } 
 				    tbtn := material.Button(th, btndot, ".")
@@ -253,30 +279,56 @@ func run(window *app.Window) error {
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btn0.Clicked(gtx){
-					log.Println("0")
-					strbelow = strbelow + "0"
+					strBelow = strBelow + "0"
 				    } 
 				    tbtn := material.Button(th, btn0, "0")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btneq.Clicked(gtx){
-					log.Println("=")
-					strbelow = strbelow + "="
+					
+					if len(strBelow) > 0 {
+					    strArr = append(strArr, strBelow)
+					} 
+					log.Println(strArr)
+					if len(strArr) > 0 {
+					    
+					    log.Println(strArr)
+					    strBelow = ""
+					    if isOperand(strArr[len(strArr) - 1]) {
+						log.Println(strArr)
+						strArr = strArr[:len(strArr) - 1]
+						log.Println(strArr)
+					    }
+					    if len(strArr) == 1 {
+						strArr = make([]string, 0)
+					    } else {
+						strArr = calculate(strArr)
+						strArr = strArr[:1]
+					    }
+					}
+					
 				    } 
 				    tbtn := material.Button(th, btneq, "=")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
 				    return tbtn.Layout(gtx)})
 			    }),
 			    layout.Flexed(0.25, func (gtx layout.Context) layout.Dimensions {
-				return layout.UniformInset(unit.Dp(5)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
+				return layout.UniformInset(unit.Dp(10)).Layout(gtx, func (gtx layout.Context) layout.Dimensions{	
 				    for btnsum.Clicked(gtx){
-					log.Println("+")
-					strbelow = strbelow + "+"
+					if len(strBelow) > 0 {
+					    if len(strArr) == 1 {
+						strArr = make([]string, 0)
+					    }
+					    strArr = append(strArr, strBelow)
+					    strArr = append(strArr, "+")
+					    strBelow = ""
+					    strArr = calculate(strArr)
+					}
 				    } 
 				    tbtn := material.Button(th, btnsum, "+")
 				    tbtn.Background = color.NRGBA{R: 20, G: 20, B: 20, A: 220}
@@ -297,6 +349,58 @@ func run(window *app.Window) error {
     }
 }
 
+func  calculate(s []string) []string {
+
+    log.Println(len(s))
+    if len(s) < 3 || waitMulDiv(s) {
+	return s
+    }
+    
+    if  len(s) > 4 {
+	s[2] = partialCalc(s[2:5])
+    } 
+    if  len(s) >= 3  {
+
+	s[0] =  partialCalc(s[:3])
+	if isOperand(s[len(s)-1]) {
+	    s[1] = s[len(s)-1]
+	}
+    }
+    return s[:2]
+}
+func isOperand(s string) bool {
+
+    return s == "x" || s == "/" || s == "+" || s == "-"
+}
+
+func waitMulDiv(s []string) bool {
+
+    return len(s) == 4 && (s[3] == "x" || s[3] == "/") && (s[1] == "+" || s[1] == "-")
+
+}
+
+func partialCalc (s []string) string {
+    first, err1 := strconv.ParseFloat(s[0], 32)
+    second, err2 := strconv.ParseFloat(s[2], 32)
+    if err1 != nil || err2 != nil {
+	log.Panic("No value to calculate!")
+    }
+    var temp float64	    	
+    switch s[1] {
+    case "x":
+	temp = first*second	
+    case "/":
+	temp = first/second
+    case "-":
+	temp = first-second
+    case "+":
+	temp = first+second
+    }
+	
+
+    return fmt.Sprintf("%f", temp)  
+	
+}
 
 func drawTitle(th *material.Theme, gtx *layout.Context) {
 
